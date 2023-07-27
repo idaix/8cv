bb796b2cac2fea00e8763f43f39ab3c0f1ed5fda
daishek <daichekkal@gmail.com>
2023-07-27 21:33:12 +0100

    auth

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import X from "./x";
+
+const Login = () => {
+  return (
+    <div className="grid place-content-center p-20 gap-5">
+      <X />
+    </div>
+  );
+};
+
+export default Login;
---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,29 @@
+import NextAuth, { AuthOptions } from "next-auth";
+import Githubprovider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Githubprovider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
