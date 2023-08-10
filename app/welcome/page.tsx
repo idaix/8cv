a679704e74f2d76e7acd2d6753f94cca46bf0d5d
daishek <daichekkal@gmail.com>
2023-08-10 19:29:03 +0100

    Welcome page

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -1,5 +1,50 @@
-const Page = () => {
-  return <div>New user</div>;
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ChevronRight } from "lucide-react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+const WelcomePage = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+  return (
+    <main className="h-full grid place-content-center container mx-auto">
+      <h1 className="text-4xl font-bold">
+        Welcome{" "}
+        <span className="bg-clip-text bg-gradient-to-tr from-green-400 to-violet-600 text-transparent">
+          Talentest
+        </span>
+      </h1>
+      <h1 className="text-4xl font-bold">
+        Are you ready to make{" "}
+        <span className="bg-clip-text bg-gradient-to-tr from-green-400 to-violet-600 text-transparent">
+          THE BOOM
+        </span>
+      </h1>
+      <h1 className="text-4xl font-bold">
+        You are in the right{" "}
+        <span className="bg-clip-text bg-gradient-to-tr from-green-400 to-violet-600 text-transparent">
+          Place
+        </span>
+      </h1>
+      <h1 className="text-4xl font-bold">
+        Next step complete your{" "}
+        <span className="bg-clip-text bg-gradient-to-tr from-green-400 to-violet-600 text-transparent">
+          Profile
+        </span>
+      </h1>
+      <div className="mt-2 text-end">
+        <Button
+          onClick={() => router.push(`/${session?.user.id}`)}
+          variant={"outline"}
+        >
+          Continue
+          <ChevronRight className="h-4 w-4 ml-2" />
+        </Button>
+      </div>
+    </main>
+  );
 };
 
-export default Page;
+export default WelcomePage;
