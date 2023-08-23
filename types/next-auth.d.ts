8e1e8c7ba6c54e9711ee438b9e7791c5515f1ce7
daishek <daichekkal@gmail.com>
2023-08-23 12:10:52 +0100

    🛠️ fix ts typing error

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,4 +1,4 @@
-import NextAuth, { DefaultSession } from "next-auth";
+import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
@@ -6,5 +6,11 @@ declare module "next-auth" {
       id: string;
       username?: String;
     } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth" {
+  interface User {
+    username: String & DefaultUser;
   }
 }
