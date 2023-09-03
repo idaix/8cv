a67f6e2eb35ad4faa9fe20630ec540324648ef48
daishek <daichekkal@gmail.com>
2023-09-03 15:43:04 +0100

    forms are ready, waiting for the api - backend logic next* -

--- components/modals/components/contact-form.tsx
+++ components/modals/components/contact-form.tsx
@@ -1,7 +1,71 @@
 "use client";
 
-const ContactForm = () => {
-  return <div>Create a new contact</div>;
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+const formSchema = z.object({
+  type: z.string().min(1).max(50),
+  link: z.string().min(1),
+});
+
+type FormSchema = z.infer<typeof formSchema>;
+
+interface IProps {
+  // initialData?: Project;
+}
+
+const ContactForm: React.FC<IProps> = () => {
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      type: "",
+      link: "",
+    },
+  });
+  return (
+    <section className=" h-full">
+      <Form {...form}>
+        <form className="grid gap-y-3">
+          <FormField
+            name="type"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Type*</FormLabel>
+                <FormControl>
+                  <Input placeholder="Personal, Github, etc" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="link"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Link*</FormLabel>
+                <FormControl>
+                  <Input placeholder="https://example.com" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </section>
+  );
 };
 
 export default ContactForm;
--- components/modals/components/project-form.tsx
+++ components/modals/components/project-form.tsx
@@ -1,7 +1,124 @@
 "use client";
 
-const ProjectForm = () => {
-  return <div>Create a new project</div>;
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Profile, Project } from "@prisma/client";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+const currentYear = new Date().getFullYear();
+
+const formSchema = z.object({
+  title: z.string().min(1).max(100),
+  year: z.number().min(2000).max(currentYear),
+  link: z.string().regex(/^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i, {
+    message: "Invalid URL",
+  }),
+  client: z.string(),
+  description: z.string(),
+});
+
+type FormSchema = z.infer<typeof formSchema>;
+
+interface IProps {
+  // initialData?: Project;
+}
+
+const ProjectForm: React.FC<IProps> = () => {
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      link: "",
+      client: "",
+      description: "",
+    },
+  });
+  return (
+    <section className=" h-full">
+      <Form {...form}>
+        <form className="grid gap-y-3">
+          <FormField
+            name="title"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Title*</FormLabel>
+                <FormControl>
+                  <Input placeholder="Woooof" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="year"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Year*</FormLabel>
+                <FormControl>
+                  <Input placeholder={currentYear.toString()} {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="client"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Company or client</FormLabel>
+                <FormControl>
+                  <Input placeholder="Acme inc." {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="link"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Link to project</FormLabel>
+                <FormControl>
+                  <Input placeholder="https://example.com" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="description"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="Tell us about your project"
+                    {...field}
+                  ></Textarea>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </section>
+  );
 };
 
 export default ProjectForm;
