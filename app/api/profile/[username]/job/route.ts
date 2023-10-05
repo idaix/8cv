a3f9b1e2087e0c654e22d3d36f9593a48175a2de
daishek <daichekkal@gmail.com>
2023-10-05 19:43:46 +0100

    job creation endpoint -not ready-

---
+++ app/api/profile/[username]/job/route.ts
@@ -0,0 +1,74 @@
+import getCurrentUser from "@/app/api/actions/getCurrentUser";
+import prismadb from "@/lib/prismadb";
+import { NextResponse } from "next/server";
+
+export async function POST(
+  request: Request,
+  { params }: { params: { username: string } }
+) {
+  try {
+    const body = await request.json();
+    const {
+      companyName,
+      companyURL,
+      companyLogo,
+      jobTitle,
+      link,
+      description,
+      type,
+      location,
+      keywords,
+    } = body;
+    if (
+      !params.username ||
+      !companyName ||
+      companyURL ||
+      companyLogo ||
+      jobTitle
+    ) {
+      return new NextResponse("Missing fields id are required!", {
+        status: 400,
+      });
+    }
+
+    const currentUser = await getCurrentUser();
+    if (!currentUser) {
+      return new NextResponse("Unauthenticated, authentication required!", {
+        status: 400,
+      });
+    }
+
+    const profile = await prismadb.profile.findFirst({
+      where: {
+        username: params.username,
+        userId: currentUser.id,
+      },
+    });
+    if (!profile) {
+      return new NextResponse("No profile found with this informations", {
+        status: 400,
+      });
+    }
+
+    const job = await prismadb.job.create({
+      data: {
+        companyLogo,
+        companyName,
+        companyURL,
+        jobTitle,
+        link,
+        description,
+        keywords,
+        location,
+        profileId: profile.username,
+        type,
+      },
+    });
+
+    return NextResponse.json(job);
+  } catch (error) {
+    console.log("jJOB_CREATION_ERROR", error);
+
+    return new NextResponse("Something went wrong");
+  }
+}
